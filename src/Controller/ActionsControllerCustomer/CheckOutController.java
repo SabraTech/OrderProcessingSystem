d14047262bf8dc6ef007d45fa6a7c5411a8d981a
sabra <mohamed2006fs@gmail.com>
2017-05-08 18:32:10 +0200

    Testing 5 and working on jasper !!!

--- src/Controller/ActionsControllerCustomer/CheckOutController.java
+++ src/Controller/ActionsControllerCustomer/CheckOutController.java
@@ -25,17 +25,29 @@ public class CheckOutController {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("call update_quantities();");
-            StringBuilder sb1 = new StringBuilder();
-            sb1.append("call update_sold()");
             try {
+                Engine.CONNECTION.setAutoCommit(false);
+                StringBuilder sb = new StringBuilder();
+                sb.append("call update_quantities();");
+                StringBuilder sb1 = new StringBuilder();
+                sb1.append("call update_sold()");
                 Engine.STATEMENT.execute(sb.toString());
                 JOptionPane.showMessageDialog(null, "Check out completed successfully!");
                 Engine.STATEMENT.execute(sb1.toString());
                 JOptionPane.showMessageDialog(null, "Sales added successfully!");
+                Engine.CONNECTION.commit();
             } catch (Exception e1) {
+                try {
+                    Engine.CONNECTION.rollback();
+                }catch(Exception e2){
+
+                }
                 JOptionPane.showMessageDialog(null, "Error in check out!");
+            }
+            try {
+                Engine.CONNECTION.setAutoCommit(true);
+            }catch (Exception e3){
+
             }
         }
     }
